3edec82f494dba4f4274644a83c3ac235a0622b3
hattuan-1811 <hatuan.dhti15a15hn@sv.uneti.edu.vn>
2024-11-10 16:27:58 +0700

    update

--- QuanLyCuaHangBanXeDap/Program.cs
+++ QuanLyCuaHangBanXeDap/Program.cs
@@ -16,7 +16,7 @@ namespace QuanLyCuaHangBanXeDap
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ());
+            Application.Run(new  frmKhachHang());
         }
     }
 }
