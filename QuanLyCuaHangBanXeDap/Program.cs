5c1b7588a3905dd895d7c072e6d077448e816ab8
Le Van Quyen <levanquyencontacts@gmail.com>
2024-11-21 23:14:14 +0700

    update

--- QuanLyCuaHangBanXeDap/Program.cs
+++ QuanLyCuaHangBanXeDap/Program.cs
@@ -16,7 +16,7 @@ namespace QuanLyCuaHangBanXeDap
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new main());
+            Application.Run(new dangnhap());
         }
     }
 }
